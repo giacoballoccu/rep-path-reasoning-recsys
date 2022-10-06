9f040057489c2f1d9be32f93bc5a9a8653065736
giacoballoccu <giacoballoccu@gmail.com>
2022-10-06 23:35:59 +0200

    Add support for path evaluation

---
+++ reasoning_path.py
@@ -0,0 +1,146 @@
+from reasoning_path_utils import *
+
+
+class ReasoningPath:
+    def __init__(self, path, score, prob, lir, sep, path_type, pattern):
+        self.path = path
+        self.score = score
+        self.prob = prob
+        self.lir = lir
+        self.sep = sep
+        self.path_type = path_type
+        self.pattern = pattern
+
+    def len(self):
+        len = 0
+        for s in self.path:
+            if type(s) != str:
+                s = str(s)
+            if s.isnumeric():
+                len += 1
+        return len
+
+
+class TopkReasoningPaths:
+    def __init__(self, dataset_name, paths):
+        self.dataset_name = dataset_name
+        self.topk = paths
+        self.max_path_type = get_no_path_types_in_kg(dataset_name)
+
+    # Time recency of linking interaction
+    def topk_lir(self):
+        lirs_topk = []
+        for path in self.topk:
+            lirs_topk.append(path.lir)
+        return np.mean(lirs_topk)
+
+    # Popularity of shared entity
+    def topk_sep(self):
+        seps_topk = []
+        for path in self.topk:
+            seps_topk.append(path.sep)
+        return np.mean(seps_topk)
+
+    # Diversity of linked interaction
+    def topk_lid(self):
+        unique_linking_interaction = set()
+        for path in self.topk:
+            linked_interaction_id, _ = get_linked_interaction_triple(path)
+            unique_linking_interaction.add(linked_interaction_id)
+        return len(unique_linking_interaction) / self.k
+
+    # Diversity of shared entities
+    def topk_sed(self):
+        unique_shared_entity = set()
+        for path in self.topk:
+            shared_entity_id, _ = get_shared_entity_tuple(path)
+            unique_shared_entity.add(shared_entity_id)
+        return len(unique_shared_entity) / self.k
+
+    # Diversity of path type
+    def topk_ptd(self):
+        unique_path_type = set()
+        for path in self.topk:
+            unique_path_type.add(path.path_type)
+        return len(unique_path_type) / max(self.k, self.max_path_type)
+
+    def topk_ptc(self):
+        def simpson_index(topk):
+            n_path_for_patterns = {k: 0 for k in set(get_path_types_in_kg(self.dataset_name))}
+            N = 0
+            for path in topk:
+                path_type = self.path_type
+                n_path_for_patterns[path_type] += 1
+                N += 1
+            numerator = 0
+            for path_type, n_path_type_ith in n_path_for_patterns.items():
+                numerator += n_path_type_ith * (n_path_type_ith - 1)
+
+            if N * (N - 1) == 0:
+                return 0
+            return 1 - (numerator / (N * (N - 1)))
+
+        ETC = simpson_index(self.topk)
+        return ETC
+
+    # Diversity of linked interaction type
+    def topk_litd(self):
+        unique_linking_interaction_types = set()
+        for path in self.topk:
+            _, _, linked_interaction_type = get_linked_interaction_triple(path)
+            unique_linking_interaction_types.add(linked_interaction_type)
+        return len(unique_linking_interaction_types) / self.k
+
+    # Diversity of shared entities type
+    def topk_setd(self):
+        unique_shared_entity_type = set()
+        for path in self.topk:
+            _, shared_entity_type = get_shared_entity_tuple(path)
+            unique_shared_entity_type.add(shared_entity_type)
+        return len(unique_shared_entity_type) / self.k
+
+    """
+     # (self_loop user 0) (watched movie 2408) (watched user 1953) (watched movie 277) #hop3
+     # (self_loop user 0) (mention word 2408) (described_as product 1953) (self_loop product 1953) #hop2
+     def template_single(self):
+          path = self.path
+          if path[0] == "self_loop":
+               path = path[1:]
+
+          path_length = path.len()
+          if path_length == 3:
+               path = path[:-1]
+          path_elements = {}
+          for i in range(1, len(path)):
+               relation, entity_type, entity_id = path[i]
+               path_elements[f"r_{i - 1}"] = relation
+               path_elements[f"e_{i - 1}"] = entity2name[entity_type][
+                    entity_id] if entity_type != "user" else f"user_{entity_id}"
+
+          if path_length == 4:
+               return f"{path_elements['e_4']} is recommend to you because you {path_elements[f'r_0']} " \
+                      f"{path_elements[f'e_1']} also {path_elements[f'r_3']} by {path_elements[f'e_2']}"
+
+          elif path_length(path) == 3:
+               _, uid, rel_0, e_type_1, e_1, rel_1, _, pid = path
+               return f"{path_elements['e_3']} is recommend to you because is {path_elements['r_2']} " \
+                      f"with {path_elements['e_1']} that you previously {path_elements['r_0']}"
+     """
+
+def pathfy(dataset_name, uid_paths):
+    LIR_matrix = load_LIR_matrix()
+    SEP_matrix = load_SEP_matrix()
+    topk_reasoning_paths = {}
+    for uid, path_triplets in uid_paths.items():
+        curr_reasoning_paths = []
+        for path_triplet in path_triplets:
+            score, prob, path = path_triplet
+            linked_interaction_id, linked_interaction_rel = get_linked_interaction_triple(path)
+            shared_entity_id, shared_entity_type = get_shared_entity_tuple(path)
+            lir = LIR_matrix[linked_interaction_rel][linked_interaction_id]
+            sep = SEP_matrix[shared_entity_type][shared_entity_id]
+            path_type = get_path_type(path)
+            pattern = get_path_pattern(path)
+            curr_reasoning_paths.append(ReasoningPath(path, score, prob, lir, sep, path_type, pattern))
+        topk_reasoning_paths[uid] = TopkReasoningPaths(dataset_name, curr_reasoning_paths)
+    return topk_reasoning_paths
---
+++ reasoning_path_utils.py
@@ -0,0 +1,198 @@
+from collections import defaultdict
+
+import numpy as np
+from utils import *
+from easydict import EasyDict as edict
+
+LIR = "lir"
+SEP = "sep"
+PTD = "ptd"
+LID = "lid"
+SED = "sed"
+PTC = "ptc"
+PPT = "ppt"
+PATH_QUALITY_METRICS = [LIR, SEP, PTD, LID, SED, PTC, PPT]
+
+def entity2plain_text(dataset_name, model_name):
+    entity2name = entity2plain_text(dataset_name, model_name)
+    return entity2name
+
+#(self_loop user 0) (watched movie 2408) (watched user 1953) (watched movie 277) #hop3
+#(self_loop user 0) (mention word 2408) (described_as product 1953) (self_loop product 1953) #hop2
+def get_linked_interaction_triple(path):
+    linked_interaction_id, linked_interaction_rel, linked_interaction_type = path[1][-1], path[1][0], path[1][1]
+    return linked_interaction_id, linked_interaction_rel
+
+def get_shared_entity_tuple(path):
+    path_type = path[-1][0]
+    if path_type == 'self_loop':  # Handle size 3
+        shared_entity_id, shared_entity_type = path[-2][-1], path[-2][1]
+        return shared_entity_id, shared_entity_type
+    shared_entity_id, shared_entity_type = path[-2][-1], path[-2][1]
+    return shared_entity_id, shared_entity_type
+
+def get_path_type(path):
+    path_type = path[-1][0]
+    if path_type == 'self_loop': #Handle size 3
+        path_type = path[-2][0]
+    return path_type
+
+def get_path_pattern(path):
+    return [path_tuple[0] for path_tuple in path[1:]]
+
+def get_path_types_in_kg(dataset_name):
+    df_kg = pd.DataFrame(os.join(get_data_dir(dataset_name), "kg_final.txt"), sep="\t", header=True)
+    return list(df_kg.kg.relations.unique())
+
+def get_no_path_types_in_kg(dataset_name):
+    df_kg = pd.DataFrame(os.join(get_data_dir(dataset_name), "kg_final.txt"), sep="\t", header=True)
+    return len(df_kg.kg.relations.unique())
+
+def load_LIR_matrix(self):
+    lir_matrix_filepath = os.path.join(self.precomputed_folderpath, self.dataset_name, "LIR_matrix.pkl")
+    lir_words_matrix_filepath = os.path.join(self.precomputed_folderpath, self.dataset_name, "LIR_matrix_words.pkl")
+    if os.path.isfile(lir_matrix_filepath):
+        print("Loading pre-computed LIR-matrix")
+        with open(lir_matrix_filepath, 'rb') as f:
+            self.LIR_matrix = pickle.load(f)
+        f.close()
+
+        if self.dataset_name != "ml1m" and self.dataset_name != "lastfm":
+            with open(lir_words_matrix_filepath, 'rb') as f:
+                self.LIR_matrix_words = pickle.load(f)
+            f.close()
+    else:
+        print("Generating LIR-matrix")
+        self.generate_LIR_matrix()
+        with open(os.path.join(self.precomputed_folderpath, self.dataset_name, 'LIR_matrix.pkl'), 'wb') as f:
+            pickle.dump(self.LIR_matrix, f)
+        f.close()
+        with open(os.path.join(self.precomputed_folderpath, self.dataset_name, 'LIR_matrix_words.pkl'), 'wb') as f:
+            pickle.dump(self.LIR_matrix_words, f)
+        f.close()
+
+
+def load_SEP_matrix(self):
+    sep_matrix_filepath = os.path.join(self.precomputed_folderpath, self.dataset_name, "SEP_matrix.pkl")
+    if os.path.isfile(sep_matrix_filepath):
+        print("Loading pre-computed SEP-matrix")
+        with open(sep_matrix_filepath, 'rb') as f:
+            self.SEP_matrix = pickle.load(f)
+        f.close()
+    else:
+        print("Generating SEP-matrix")
+        self.generate_SEP_matrix()
+        with open(os.path.join(self.precomputed_folderpath, self.dataset_name, 'SEP_matrix.pkl'), 'wb') as f:
+            pickle.dump(self.SEP_matrix, f)
+        f.close()
+
+def get_dataset_pid2model_kg_pid(dataset_name, model_name):
+    model_data_dir = get_model_data_dir(dataset_name, model_name)
+    file = open(os.path.join(model_data_dir, "mappings/product_mappings.txt", "r")) #TODO CHECK if we want that filename
+    csv_reader = csv.reader(file, delimiter='\t')
+    dataset_pid2model_kg_pid = {}
+    next(csv_reader, None)
+    for row in csv_reader:
+        dataset_pid2model_kg_pid[row[1]] = int(row[0])
+    file.close()
+    return dataset_pid2model_kg_pid
+
+def get_dataset_uid2model_kg_uid(dataset_name, model_name):
+    model_data_dir = get_model_data_dir(dataset_name, model_name)
+    file = open(os.path.join(model_data_dir, "mappings/user_mappings.txt", "r"))
+    reader = csv.reader(file, delimiter="\t")
+    dataset_uid2model_kg_pid = {}
+    next(reader, None)
+    for row in reader:
+        uid_review = row[1]
+        uid_kg = int(row[0])
+        dataset_uid2model_kg_pid[uid_review] = uid_kg
+    return dataset_uid2model_kg_pid
+
+def get_interaction2timestamp_map(dataset_name, model_name):
+    data_dir = get_model_data_dir(dataset_name, model_name)
+    #Load if already computated
+    metadata_filepath = os.path.join(data_dir, "time_metadata.pkl")
+    if os.path.isfile(metadata_filepath):
+        with open(metadata_filepath, 'rb') as f:
+            user2pid_time_tuple = pickle.load(f)
+        f.close()
+        return user2pid_time_tuple
+    #Compute and save if not yet
+    else:
+        user2pid_time_tuple = defaultdict(list)
+        dataset2kg = get_dataset_pid2model_kg_pid(dataset_name)
+        file = open(os.path.join(data_dir, "train.txt"), 'r')
+        csv_reader = csv.reader(file, delimiter=' ')
+        uid_mapping = get_dataset_uid2model_kg_uid(dataset_name)
+        for row in csv_reader:
+            uid = uid_mapping[row[0]]
+            pid = row[1]
+            pid_model_kg = dataset2kg[pid]
+            timestamp = int(row[3])
+            user2pid_time_tuple[uid].append((PRODUCT, pid_model_kg, timestamp))
+            if dataset_name in DATASETS_WITH_WORDS:
+                words = row[3:]
+                for word in words:
+                    user2pid_time_tuple[uid].append((WORD, word, timestamp))
+        with open(os.path.join(metadata_filepath), 'wb') as f:
+            pickle.dump((user2pid_time_tuple), f)
+        f.close()
+        return user2pid_time_tuple
+
+def generate_LIR_matrix(dataset_name, model_name):
+    def normalized_ema(values):
+        if max(values) == min(values):
+            values = np.array([i for i in range(len(values))])
+        else:
+            values = np.array([i for i in values])
+        values = pd.Series(values)
+        ema_vals = values.ewm(span=len(values)).mean().tolist()
+        min_res = min(ema_vals)
+        max_res = max(ema_vals)
+        return [(x - min_res) / (max_res - min_res) for x in ema_vals]
+
+    uid_timestamp = get_interaction2timestamp_map(dataset_name, model_name)
+    LIR_matrix = {uid: {} for uid in uid_timestamp.keys()}
+    for uid in uid_timestamp.keys():
+        linked_interaction_types = [PRODUCT]
+        if dataset_name in DATASETS_WITH_WORDS:
+            linked_interaction_types = [PRODUCT, WORD]
+        for linked_entity_type in linked_interaction_types:
+            interactions = [type_id_time for type_id_time in uid_timestamp[uid] if type_id_time[0] == linked_entity_type]
+            interactions.sort(key=lambda x: x[2])
+            if len(uid_timestamp[uid]) <= 1:  # Skips users with only one review in train (can happen with lastfm)
+                continue
+            ema_timestamps = normalized_ema(interactions)
+            pid_lir = {}
+            for i in range(len(interactions)):
+                pid = interactions[i][1]
+                lir = ema_timestamps[i]
+                pid_lir[pid] = lir
+            LIR_matrix[uid][linked_entity_type] = pid_lir
+    return LIR_matrix
+
+def generate_SEP_matrix(dataset_name, model_name):
+    def normalized_ema(values):
+        if max(values) == min(values):
+            values = np.array([i for i in range(len(values))])
+        else:
+            values = np.array([i for i in values])
+        values = pd.Series(values)
+        ema_vals = values.ewm(span=len(values)).mean().tolist()
+        min_res = min(ema_vals)
+        max_res = max(ema_vals)
+        return [(x - min_res) / (max_res - min_res) for x in ema_vals]
+    # Precompute entity distribution
+    SEP_matrix = {}
+    degrees = load_kg(dataset_name, model_name).degrees
+    for type, eid_degree in degrees.items():
+        eid_degree_tuples = list(zip(eid_degree.keys(), eid_degree.values()))
+        eid_degree_tuples.sort(key=lambda x: x[1])
+        ema_es = normalized_ema([x[1] for x in eid_degree_tuples])
+        pid_weigth = {}
+        for idx in range(len(ema_es)):
+            pid = eid_degree_tuples[idx][0]
+            pid_weigth[pid] = ema_es[idx]
+
+        SEP_matrix[type] = pid_weigth
