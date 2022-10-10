7db83795659a5ed2a2f839a8c2a3e2e0922ba921
giacoballoccu <giacoballoccu@gmail.com>
2022-10-10 19:09:17 +0200

    Added rec quality metrics

--- rec_quality_metrics.py
+++ rec_quality_metrics.py
@@ -14,18 +14,21 @@ NOVELTY = "novelty"
 CFAIRNESS = "cfairness"
 PFAIRNESS = "pfairness"
 
-REC_QUALITY_METRICS_TOPK = [NDCG, MMR, SERENDIPITY, DIVERSITY, NOVELTY] #CHECK EVERYTIME A NEW ONE IS IMPLEMENTED IF IS LOCAL O GLOBAL
-REC_QUALITY_METRICS_GLOBAL = [COVERAGE]
-FAIRNESS_METRICS = [CFAIRNESS, PFAIRNESS]
+REC_QUALITY_METRICS_TOPK = [NDCG, MMR, SERENDIPITY, DIVERSITY,
+                            NOVELTY, PFAIRNESS]  # CHECK EVERYTIME A NEW ONE IS IMPLEMENTED IF IS LOCAL O GLOBAL
+REC_QUALITY_METRICS_GLOBAL = [COVERAGE, CFAIRNESS]
 """
 Methods
 """
+
+
 def MMR(hit_list, k):
     r = np.asfarray(hit_list)[:k]
     hit_idxs = np.nonzero(r)
     if len(hit_idxs[0]) > 0:
-        return 1 / (hit_idxs[0][0]+1)
+        return 1 / (hit_idxs[0][0] + 1)
     return 0.
+
 
 def dcg_at_k(hit_list, k, method=1):
     r = np.asfarray(hit_list)[:k]
@@ -38,11 +41,13 @@ def dcg_at_k(hit_list, k, method=1):
             raise ValueError('method must be 0 or 1.')
     return 0.
 
+
 def ndcg_at_k(hit_list, k, method=0):
     dcg_max = dcg_at_k(sorted(hit_list, reverse=True), k, method)
     if not dcg_max:
         return 0.
     return dcg_at_k(hit_list, k, method) / dcg_max
+
 
 def measure_rec_quality(path_data):
     # Evaluate only the attributes that have been chosen and are avaiable in the chosen dataset
@@ -88,26 +93,28 @@ def measure_rec_quality(path_data):
         precision = hit_num / len(pred_list)
         hit = 1.0 if hit_num > 0.0 else 0.0
         mmr = MMR(hit_list, k)
-        #f1 = (2*precision*recall)/(precision+recall)
+        # f1 = (2*precision*recall)/(precision+recall)
         # Based on attribute
         for attribute in attribute_list.keys():
             if uid not in attribute_list[attribute][0]: continue
             attr_value = attribute_list[attribute][0][uid]
-            if attr_value not in attribute_list[attribute][1]: continue #Few users may have the attribute missing (LASTFM)
+            if attr_value not in attribute_list[attribute][
+                1]: continue  # Few users may have the attribute missing (LASTFM)
             attr_name = attribute_list[attribute][1][attr_value]
             metrics["ndcg"][attr_name].append(ndcg)
             metrics["recall"][attr_name].append(recall)
             metrics["precision"][attr_name].append(precision)
             metrics["hr"][attr_name].append(hit)
             metrics["mmr"][attr_name].append(mmr)
-            #metric["f1"][attr_name].append(f1)
+            # metric["f1"][attr_name].append(f1)
         metrics["ndcg"]["Overall"].append(ndcg)
         metrics["recall"]["Overall"].append(recall)
         metrics["precision"]["Overall"].append(precision)
         metrics["hr"]["Overall"].append(hit)
         metrics["mmr"]["Overall"].append(mmr)
-        #metrics["f1"]["Overall"].append(f1)
+        # metrics["f1"]["Overall"].append(f1)
     return metrics
+
 
 def print_rec_metrics(dataset_name, flags, metrics):
     attribute_list = get_attribute_list(dataset_name, flags)
@@ -127,6 +134,7 @@ def print_rec_metrics(dataset_name, flags, metrics):
             print("")
     print("\n")
 
+
 def print_rec_quality_metrics(avg_metrics):
     print("\n***---Recommandation Quality---***")
     print("Average for the entire user base:", end=" ")
@@ -141,11 +149,13 @@ def print_rec_quality_metrics(avg_metrics):
         print("")
     print("\n")
 
+
 def generate_latex_row(model_name, avg_metrics):
     row = [model_name]
     for metric in avg_metrics.keys():
         row.append(avg_metrics[metric][OVERALL])
     return ' & '.join(row)
+
 
 """
 Beyond Accuracy
@@ -154,20 +164,50 @@ Beyond Accuracy
 """
     Catalog coverage https://dl.acm.org/doi/pdf/10.1145/2926720
 """
+
+
 def coverage(recommended_items_by_group, n_items_in_catalog):
     group_metric_value = {}
     for group, item_set in recommended_items_by_group.items():
         group_metric_value[group] = len(item_set) / n_items_in_catalog
     return group_metric_value
 
+
 def serendipity_at_k():
     pass
 
-def diversity_at_k():
-    pass
 
-def novelty_at_k():
-    pass
+def diversity_at_k(topk_items, pid2genre):
+    diversity_items_tok = [pid2genre[pid] for pid in topk_items]
+    return np.mean(diversity_items_tok)
 
-#REC_QUALITY_METRICS = [NDCG, MMR, SERENDIPITY, COVERAGE, DIVERSITY, NOVELTY]
-#FAIRNESS_METRICS = [CFAIRNESS, PFAIRNESS]
+
+def novelty_at_k(topk_items, pid2popularity):
+    novelty_items_topk = [1 - pid2popularity[pid] for pid in topk_items]
+    return np.mean(novelty_items_topk)
+
+def exposure_pfairness(topk_items, pid2provider_popularity)
+    exposure_providers_topk = [pid2provider_popularity[pid] for pid in topk_items]
+    return np.mean(exposure_providers_topk)
+
+def consumer_fairness(metrics_distrib, avg_metrics):
+    fairness_metrics = {}
+
+    # Compute consumer fairness
+    fairness_metrics[CFAIRNESS] = {}
+    for metric, group_values in avg_metrics.items():
+        if len(group_values) == 2:
+            group1, group2 = list(group_values.keys())
+            statistically_significant = statistical_test(metrics_distrib[metric][group1], metrics_distrib[metric][group2])
+            fairness_metrics[CFAIRNESS][metric] = (group1, group2, avg_metrics[metric][group1] -
+                                                   avg_metrics[metric][group2], statistically_significant)
+        if len(group_values) > 2:
+            pairwise_diffs = []
+            for group1 in group_values.keys():
+                for group2 in group_values.keys():
+                    if group1 != group2:
+                        pairwise_diffs.append(group_values[group1] - group_values[group2])
+                fairness_metrics[CFAIRNESS][metric] = (np.NAN, np.NAN, np.mean(pairwise_diffs), np.NAN)
+
+# REC_QUALITY_METRICS = [NDCG, MMR, SERENDIPITY, COVERAGE, DIVERSITY, NOVELTY]
+# FAIRNESS_METRICS = [CFAIRNESS, PFAIRNESS]
