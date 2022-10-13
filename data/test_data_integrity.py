a0cb2ac1d362e8bf132662e984936a6222784aaf
giacoballoccu <giacoballoccu@gmail.com>
2022-10-13 16:48:26 +0200

    Added more tests

--- data/test_data_integrity.py
+++ data/test_data_integrity.py
@@ -3,32 +3,69 @@ import argparse
 import pandas as pd
 
 def test_dataset_integrity(dataset_name):
-    i2kg_df = pd.read_csv(f"{dataset_name}/preprocessed/i2kg_map.txt", sep="\t")
+    i2kg_filepath = f"{dataset_name}/preprocessed/i2kg_map.txt"
+    i2kg_df = pd.read_csv(i2kg_filepath, sep="\t")
     if i2kg_df.shape[0] != i2kg_df.pid.unique().shape[0]:
         print("i2kg contains duplicates")
+        drop_i2kg_map_duplicates(i2kg_filepath, i2kg_df)
 
-    products_df = pd.read_csv(f"{dataset_name}/preprocessed/products.txt", sep="\t")
+    products_filepath = f"{dataset_name}/preprocessed/products.txt"
+    products_df = pd.read_csv(products_filepath, sep="\t")
     if i2kg_df.shape[0] != products_df.shape[0]:
         print("i2kg/product len missmatch")
+        drop_products_not_in_i2kg(products_filepath, products_df, i2kg_df)
 
-    interactions_df = pd.read_csv(f"{dataset_name}/preprocessed/ratings.txt", sep="\t")
+    interactions_filepath = f"{dataset_name}/preprocessed/ratings.txt"
+    interactions_df = pd.read_csv(interactions_filepath, sep="\t")
     if interactions_df[~interactions_df.pid.isin(i2kg_df.pid)].shape[0] > 0:
         print("Ratings contain interactions that involve a removed product or user")
+        remove_interactions_with_removed_product(interactions_filepath, interactions_df, i2kg_df)
 
-    entity_df = pd.read_csv(f"{dataset_name}/preprocessed/e_map.txt", sep="\t", names=["eid", "name", "entity"]).iloc[1:, :]
+    e_map_filepath = f"{dataset_name}/preprocessed/e_map.txt"
+    entity_df = pd.read_csv(e_map_filepath, sep="\t", dtype="object", names=["eid", "name", "entity"])
     entity_item = entity_df[entity_df.entity.isin(i2kg_df.entity)]
-    other_entities = entity_df[~entity_df.entity.isin(i2kg_df.entity)]
     if entity_item.shape[0] != i2kg_df.shape[0]:
         print("missing items in e_map")
+        #drop_triplets_with_not_existing_tails(e_map_filepath, )
 
-    triplets_df = pd.read_csv(f"{dataset_name}/preprocessed/kg_final.txt", sep="\t", dtype="object")
-    triplets_df_check = triplets_df[triplets_df.entity_tail.isin(entity_item)]
-    if triplets_df_check.shape[0] > 0:
+    print(entity_df.shape[0], entity_df.entity.unique().shape[0])
+    if entity_df.shape[0] != entity_df.entity.unique().shape[0]:
+        print("e_map contains duplicates")
+        entity_df.drop_duplicates(subset="entity", inplace=True)
+        if entity_df.entity.isnull().sum() != 0:
+            entity_df.dropna(axis=0, inplace=True)
+        entity_df.to_csv(e_map_filepath, sep="\t", index=False)
+    other_entities = entity_df[~entity_df.entity.isin(i2kg_df.entity)]
+    assert other_entities.shape[0] + entity_item.shape[0] == entity_df.shape[0]
+
+    kg_final_filename = f"{dataset_name}/preprocessed/kg_final.txt"
+    triplets_df = pd.read_csv(kg_final_filename, sep="\t", dtype="object")
+    cleaned_triplets = triplets_df[~triplets_df.entity_tail.isin(entity_item.eid)]
+    print(triplets_df.shape[0], cleaned_triplets.shape[0])
+    if triplets_df.shape[0] != cleaned_triplets.shape[0]:
+        cleaned_triplets.to_csv(kg_final_filename, sep="\t", index=False)
         print("Triplets contain corrupted triplets with a item as tail")
 
-    x = triplets_df[triplets_df.entity_tail.isin(other_entities)]
-    if triplets_df.shape[0] != x.shape[0]:
-        print("Triplets contain corrupted triplets with a not existing entity tail")
+    cleaned_triplets = triplets_df[triplets_df.entity_tail.isin(other_entities.eid)]
+    if triplets_df.shape[0] != cleaned_triplets.shape[0]:
+        drop_triplets_with_corrupted_tail(kg_final_filename, cleaned_triplets)
+        print(f"Removed {triplets_df.shape[0] - cleaned_triplets.shape[0]} corrupted triplets")
+
+def drop_triplets_with_corrupted_tail(kg_final_filename, cleaned_triplets):
+    print("Triplets contain corrupted triplets with a not existing entity tail")
+    cleaned_triplets.to_csv(kg_final_filename, sep="\t", index=False)
+
+def drop_i2kg_map_duplicates(i2kg_filepath, i2kg_df):
+    i2kg_df.drop_duplicates(subset="pid", inplace=True)
+    i2kg_df.to_csv(i2kg_filepath, sep="\t", index=False)
+
+def drop_products_not_in_i2kg(products_filepath, products_df, i2kg_df):
+    products_df = products_df[products_df.pid.isin(i2kg_df.pid)]
+    products_df.to_csv(products_filepath, sep="\t", index=False)
+
+def remove_interactions_with_removed_product(interactions_filepath, interactions_df, i2kg_df):
+    interactions_df = interactions_df[~interactions_df.pid.isin(i2kg_df.pid)]
+    interactions_df.to_csv(interactions_filepath, sep="\t", index=False)
 
 def main():
     parser = argparse.ArgumentParser()
