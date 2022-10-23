7b4591f066b740a4d8bf6c517d35112dfa932b37
chris1nexus <chris.2nexus@gmail.com>
2022-10-23 06:08:58 +0200

    Add build dataset script

--- build_datasets.sh
+++ build_datasets.sh
@@ -1,6 +1,6 @@
 for dataset in ml1m lfm1m ;
   do
-  	for model in cke cfkg kgat fm nfm bprmf pgpr ucpr cafe ;
+  	for model in cke cfkg kgat fm nfm bprmf pgpr ucpr cafe transe;
 		do
 			python3 map_dataset.py --data $dataset --model $model
 		done    	
