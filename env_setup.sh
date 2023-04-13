5a162f77e98a6604b24634e30ae7c41d5300fa0b
Christian Cancedda <chris@Christians-MBP.station>
2023-04-13 03:54:48 +0200

    Change folder location pretrained transe

--- env_setup.sh
+++ env_setup.sh
@@ -3,5 +3,5 @@ pip install .
 pip install gdown
 gdown https://drive.google.com/uc?id=14WaIJHgsUuPkW8_XzN6PeBjZq15Zdt4V
 unzip data.zip
-gdown https://drive.google.com/uc?id=1iRVAgqHKpHSaWfkmUVFy5XL0m7YBWyXV
+gdown https://drive.google.com/uc?id=1zFP0Cx_SdS89V7EMkFYAaaCEOfk2LwZA #1iRVAgqHKpHSaWfkmUVFy5XL0m7YBWyXV
 unzip pretrained.zip
